0a2a0f3b7e079cdadcd3fc3aee8934ca384f534e
yunomer <ashfaqm@uoguelph.ca>
2018-12-11 10:47:06 -0500

    Fixed date batching

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 def main():
     start = 0
     year_low = 2007
-    year_high = 2009
+    year_high = 2018
     #   This is our year combination
     #   2007 - 2008 => Index 0
     #   2009 - 2010 => Index 1
@@ -21,53 +21,30 @@ def main():
     #   2015 - 2016 => Index 4
     #   2017 - 2018 => Index 5
     for i in range(0, 6):
+        start = 0
         for j in range(0, 990):
             delay = random.randrange(3, 15)
             time.sleep(delay)
             page_scrape = 'https://scholar.google.ca/scholar?start=' + str(
                 start) + '&hl=en&as_sdt=0,5&sciodt=0,5&as_ylo=' + str(year_low) + '&as_yhi=' + str(
                 year_low + 1) + '&cites=10992335886072847329&scipsc='
-            r = http.request(
-                'GET',
-                page_scrape,
-                headers={
-                    "authority": "scholar.google.ca",
-                    "method": "GET",
-                    "path": "/scholar?hl=en&as_sdt=0%2C5&sciodt=0%2C5&cites=10992335886072847329&scipsc="
-                            "&as_ylo=2007&as_yhi=2018",
-                    "scheme": "https",
-                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "en-US,en;q=0.9",
-                    "cache-control": "max-age=0",
-                    "cookie": "CONSENT=YES+CA.en+20180513-19-0; OGPC=19008862-2:19009353-2:; SID=ywaLe08D1fWoWo4vM_"
-                              "MuMUY-BD2KKUbxmk1VLh5x7ZU584e8n7z75ggsP5170Eq4iEJKog.; HSID=Agla5niMAI11ewhpo; SSID=ARe"
-                              "4bHC3LJ9J1o_G8; APISID=Tx_tbXnNiNGGwWXB/AsFjYajKMY0X5uz9K; SAPISID=3d0QNj5APzigUvfQ/AbQ"
-                              "HLnnRNIUo23otX; 1P_JAR=2018-12-06-14; GSP=LM=1544106761:S=jwEPyXj5FmtWoSnG; NID=150=kdX"
-                              "wo88eCR7VONF7hSdQwqbw9Z-W-v439oY7rtFgOdTa2UH2UnnUr690ycdDKoB-8tI6a5H9ApKlIyispXpX4GDdOp"
-                              "ECEpfnXP0Hi7SA1aTgqFZCwsbuD3k5Guv598iSm3TEUVPtm5e7-cMf3jIQNQxlad8vltxSjpxwAsnxYOnfDVP_tn"
-                              "EiGxw_YCLmd7XfAODr6Jy9clIc28XoBhNR0alh0wTr8nTlpwGKOLDXH2AUj9X-LyslADSBygCqKSFYJfyI87LkdN"
-                              "qVW_DyQ50dZosmA7FKQJN2ZPQ",
-                    "upgrade-insecure-requests": "1",
-                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                                  "(KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
-                    "x-client-data": "CJK2yQEIprbJAQjEtskBCKmdygEIqKPKAQi/p8oBGPmlygE="
-                }
-            )
-            soup = BeautifulSoup(r.data, 'html.parser')
-            citation_papers = soup.find_all('div', attrs={'class': 'gs_r gs_or gs_scl'})
+            r = get_request(page_scrape)
+            citation_papers = process_request(r)
             length = len(citation_papers)
             if length < 1:
-                print(soup)
-                print("::::::::::::::::::::DONE: Year Processed::::::::::::::::::::\n")
-                break
+                r = get_request(page_scrape)
+                citation_papers = process_request(r)
+                length = len(citation_papers)
+                if length < 1:
+                    print("::::::::::::::::::::DONE: Year Processed::::::::::::::::::::\n")
+                    break
             else:
                 print("Processing Tab: " + str(start) +
-                      "\nYear Range: " + str(year_low) + " - " + str(year_high) + "\n")
-                process_data(citation_papers, start, year_low, year_high)
+                      "\nYear Range: " + str(year_low) + " - " + str(year_low + 1) + "\n")
+                process_data(citation_papers, start, year_low, year_low + 1)
                 start = start + 10
         if year_low < year_high:
-            year_low = year_low + 1
+            year_low = year_low + 2
         else:
             print("::::::::::::::::::::DONE: Accessed Last year::::::::::::::::::::\n")
 
@@ -123,6 +100,45 @@ def process_data(data, page_number, year_st, year_ed):
         pass
 
     wb.save("data_cids.xlsx")
+
+
+def get_request(page_scrape):
+    r = http.request(
+        'GET',
+        page_scrape,
+        headers={
+            "authority": "scholar.google.ca",
+            "method": "GET",
+            "path": "/scholar?hl=en&as_sdt=0%2C5&sciodt=0%2C5&cites=10992335886072847329&scipsc="
+                    "&as_ylo=2007&as_yhi=2018",
+            "scheme": "https",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "max-age=0",
+            "cookie": "CONSENT=YES+CA.en+20180513-19-0; OGPC=19008862-2:19009353-2:; SID=ywaLe08D1fWoWo4vM_"
+                      "MuMUY-BD2KKUbxmk1VLh5x7ZU584e8n7z75ggsP5170Eq4iEJKog.; HSID=Agla5niMAI11ewhpo; SSID=ARe"
+                      "4bHC3LJ9J1o_G8; APISID=Tx_tbXnNiNGGwWXB/AsFjYajKMY0X5uz9K; SAPISID=3d0QNj5APzigUvfQ/AbQ"
+                      "HLnnRNIUo23otX; 1P_JAR=2018-12-06-14; GSP=LM=1544106761:S=jwEPyXj5FmtWoSnG; NID=150=kdX"
+                      "wo88eCR7VONF7hSdQwqbw9Z-W-v439oY7rtFgOdTa2UH2UnnUr690ycdDKoB-8tI6a5H9ApKlIyispXpX4GDdOp"
+                      "ECEpfnXP0Hi7SA1aTgqFZCwsbuD3k5Guv598iSm3TEUVPtm5e7-cMf3jIQNQxlad8vltxSjpxwAsnxYOnfDVP_tn"
+                      "EiGxw_YCLmd7XfAODr6Jy9clIc28XoBhNR0alh0wTr8nTlpwGKOLDXH2AUj9X-LyslADSBygCqKSFYJfyI87LkdN"
+                      "qVW_DyQ50dZosmA7FKQJN2ZPQ",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; P027 Build/MRA58L) AppleWebKit/537.36 "
+                          "(KHTML, like Gecko) Chrome/57.0.2987.132 Safari/537.36",
+            "x-client-data": "CJK2yQEIprbJAQjEtskBCKmdygEIqKPKAQi/p8oBGPmlygE="
+        }
+    )
+
+    return r
+
+
+def process_request(r):
+    soup = BeautifulSoup(r.data, 'html.parser')
+    citation_papers = soup.find_all('div', attrs={'class': 'gs_r gs_or gs_scl'})
+    print(soup)
+    return citation_papers
 
 
 if __name__ == '__main__':
